0a2020949fcd664b8b52a91db3f254f400ad1cf9
iamtararamgoyal <tararamgoyal4@gmail.com>
2021-10-03 16:29:55 +0530

    Day 10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hello/models/catlog.dart';
 import 'package:flutter_hello/widgets/drawer.dart';
+import 'package:flutter_hello/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   int days = 30;
   String name = "Goyal sir";
-
+final dummyList = List.generate(50, (index) => CatlogModel.item[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +14,13 @@ class HomePage extends StatelessWidget {
         title: Text("Catlog App"),
       ),
       body: Center(
-        child: Container(
-          child: Text("Hello This is my $days days course by $name"),
-        ),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index]
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
