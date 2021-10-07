06d177eae4f554b6bce344227949e5d1c47ece59
iamtararamgoyal <tararamgoyal4@gmail.com>
2021-10-07 21:44:57 +0530

    Day 14 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -37,10 +37,36 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
           child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 16,
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16),
                   itemCount: CatalogModel.items.length,
                   itemBuilder: (context, index) {
-                    return ItemWidget(item: CatalogModel.items[index]);
+                    final item = CatalogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            )),
+                        child: Image.network(item.image),
+                        footer: Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: BoxDecoration(color: Colors.black),
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ),
+                    );
                   },
                 )
               : Center(
